007b7e907bef924f0420eb834a5deaaeddd6e767
xiaoX <iam_xyb@163.com>
2020-08-04 11:03:17 +0800

    汉诺塔 递归处理

--- src/main/java/com/example/dsandalgo/class11/Code01_Hanoi.java
+++ src/main/java/com/example/dsandalgo/class11/Code01_Hanoi.java
@@ -1,5 +1,7 @@
 package com.example.dsandalgo.class11;
 
+import java.util.Stack;
+
 /**
  * created on 2020/8/3.
  * time: 15:39
@@ -100,6 +102,59 @@ public class Code01_Hanoi {
         System.out.println("=======================");
         hanoi2(3);
     }
+
+
+    public static class Record{
+        //第一步是否完成
+        //把大象装进冰箱 分成3步
+        public boolean finish1;
+        public int base;
+        public String from;
+        public String to;
+        public String other;
+    public Record(boolean f, int b, String from ,String to ,String other){
+        this.finish1 =  f;
+        this.base = b;
+        this.from = from;
+        this.to  = to ;
+        this.other = other;
+
+    }
+
+
+    }
+
+    public static void hanoi3(int n){
+        if (n < 1){
+            return;
+        }
+        Stack<Record> stack = new Stack<>();
+        stack.push(new Record(false,n,"A","C","B"));
+
+        while (!stack.isEmpty()){
+            Record cur = stack.pop();
+            if (cur.base == 1){
+                System.out.println("move 1 from "+ cur.from+" to " + cur.to );
+                if (!stack.isEmpty()){
+                    stack.peek().finish1 = true;
+                }
+            }else {
+                if (!cur.finish1){
+                    stack.push(cur);
+                    stack.push(new Record(false,cur.base-1,cur.from,cur.other,cur.to));
+                }else{
+                    System.out.println("move "+cur.base +"from "+cur.from +"to" + cur.to) ;
+                    stack.push(new Record(false,cur.base-1,cur.other,cur.to,cur.from));
+                }
+
+            }
+
+        }
+
+
+
+    }
+
 
 
 
