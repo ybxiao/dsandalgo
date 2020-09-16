e034e78312a197534d04e6e4b0aec3fc25bbb38b
iam_xyb@163.com <iam_xyb@163.com>
2020-09-16 20:23:07 +0800

    review

--- src/main/java/com/example/dsandalgo/foundationclass/class04/Code05_SortArrayDistanceLessK.java
+++ src/main/java/com/example/dsandalgo/foundationclass/class04/Code05_SortArrayDistanceLessK.java
@@ -2,11 +2,23 @@ package com.example.dsandalgo.foundationclass.class04;
 
 import java.util.PriorityQueue;
 
+/**
+ *
+ * 已知一个几乎有序的数组。几乎有序是指，如果把数组排好顺序的话，每个元素移动的距离一定不超过k，
+ * 并且k相对于数组长度来说是比较小的。
+ *
+ * 请选择一个合适的排序策略，对这个数组进行排序。
+ *
+ *
+ *
+ */
+
 public class Code05_SortArrayDistanceLessK {
 
 
     public void sortArrayDistanceLessK(int[] arr ,int k){
 
+        //默认小根堆
         PriorityQueue<Integer> heap = new PriorityQueue();
         int index =0;
         for (; index <= Math.min(arr.length-1,k)  ; index++) {
