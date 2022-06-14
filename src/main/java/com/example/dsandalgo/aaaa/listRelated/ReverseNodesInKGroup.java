31df802761afa499333604263588ecb86ecd0a28
iam_xyb@163.com <iam_xyb@163.com>
2022-06-14 23:44:13 +0800

    reverseNodesInKGroup

--- src/main/java/com/example/dsandalgo/aaaa/listRelated/ReverseNodesInKGroup.java
+++ src/main/java/com/example/dsandalgo/aaaa/listRelated/ReverseNodesInKGroup.java
@@ -9,7 +9,7 @@ package com.example.dsandalgo.aaaa.listRelated;
  * */
 public class ReverseNodesInKGroup {
 
-    public class ListNode{
+    public static class ListNode{
         int val;
         ListNode next;
         ListNode(){
@@ -65,9 +65,28 @@ public class ReverseNodesInKGroup {
 
 
     public static ListNode getKGroupEnd(int k, ListNode start){
+        System.out.println(start);
         while (--k != 0 && start != null) {
             start = start.next;
+            System.out.println(start);
         }
         return start;
     }
+
+
+    public static void main(String[] args) {
+        ListNode node1= new ListNode(1);
+
+        ListNode node2= new ListNode(2);
+        ListNode node3= new ListNode(3);
+        ListNode node4= new ListNode(4);
+        ListNode node5= new ListNode(5) ;
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+
+    }
+
+
 }
