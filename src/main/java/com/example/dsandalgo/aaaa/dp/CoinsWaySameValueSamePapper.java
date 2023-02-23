92138ee83655ff049dc4f62e009e2a372187cd98
xiaoyibo <xiaoyibo@meituan.com>
2023-02-23 22:49:56 +0800

    钱币凑钱

--- src/main/java/com/example/dsandalgo/aaaa/dp/CoinsWaySameValueSamePapper.java
+++ src/main/java/com/example/dsandalgo/aaaa/dp/CoinsWaySameValueSamePapper.java
@@ -71,7 +71,7 @@ public class CoinsWaySameValueSamePapper {
         }
         int ways = 0;
         //index位置的货币可以使用0...zhangs[index] 这么多张
-        for (int i = 0; i * coins[index] <= rest & i < zhangs[index]; i++) {
+        for (int i = 0; i * coins[index] <= rest & i <= zhangs[index]; i++) {
             ways += process(coins, zhangs, index + 1, rest - i * coins[index]);
         }
         return ways;
@@ -79,5 +79,102 @@ public class CoinsWaySameValueSamePapper {
 
 
     //todo 改造成动态规划的方法
+    //dp[i][j] 代表的含义：coins[i...n]位置上的货币自由选择，凑成钱数j的方法数
+    //
+    public static int dp1(int[] arr, int aim) {
+        if (arr == null || arr.length == 0 || aim < 0) {
+            return 0;
+        }
+        Info info = getInfo(arr);
+        int[] coins = info.coins;
+        int[] zhangs = info.zhangs;
+        int n = coins.length;
+        int[][] dp = new int[n+1][aim + 1];
+        dp[n][0] = 1;
+        for (int i = n -1; i >= 0; i--) {
+            for (int j = 0; j <= aim; j++) {
+                dp[i][j] = 0;
+                for (int k = 0; k <= zhangs[i] && k * coins[i] <= j; k++) {
+                    dp[i][j] += dp[i + 1][j - k * coins[i]];
+                }
+
+            }
+
+        }
+        return dp[0][aim];
+
+    }
+
+    //严格位置依赖的方法优化，替代掉枚举字段类型
+    public static int dp2(int[] arr, int aim){
+        if (arr == null || arr.length == 0 || aim < 0) {
+            return 0;
+        }
+        Info info = getInfo(arr);
+        int[] coins = info.coins;
+        int[] zhangs = info.zhangs;
+        int n = coins.length;
+        int[][] dp = new int[n+1][aim + 1];
+        dp[n][0] = 1;
+        //从下往上，从左往右依次去填dp[i][j]的值
+        for (int i = n-1; i >=0 ; i--) {
+            for (int rest = 0; rest <= aim; rest++) {
+                dp[i][rest] = dp[i+1][rest] ;
+                if (rest - coins[i] >= 0){
+                    dp[i][rest] += dp[i][rest - coins[i]];
+                }
+                if (rest - coins[i] * (zhangs[i] + 1) >=0){
+                    dp[i][rest] -= dp[i+1][rest - coins[i] * (zhangs[i] + 1) ];
+                }
+
+            }
+
+        }
+        return dp[0][aim];
+
+    }
+
+    public static int[] randomArray(int maxLen ,int maxValue){
+        int n = (int) (Math.random() * maxLen) + 1;
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            int v = (int) (Math.random() * maxValue) + 1;
+            arr[i] = v;
+        }
+        return arr;
+    }
+
+    public static void printArray(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i] + "");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int maxLen = 10;
+        int maxValue =20;
+        int testTimes = 100000;
+        for (int i = 0; i < testTimes; i++) {
+            int[] arr = randomArray(maxLen, maxValue);
+            int aim = (int)(Math.random() * maxValue) +1;
+            int ans1 = coinsWays(arr, aim);
+            int ans2 = dp2(arr, aim);
+            int ans3 = dp1(arr, aim);
+            if (ans1 != ans2 || ans2 != ans3){
+                System.out.println("Oops!");
+                printArray(arr);
+                System.out.println(aim);
+                System.out.println(ans1);
+                System.out.println(ans2);
+                System.out.println(ans3);
+                break;
+            }
+            
+        }
+
+        System.out.println("测试结束");
+    }
+
 
 }
